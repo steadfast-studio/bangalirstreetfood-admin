13e27c2f0be4e8af2e02020c0d6a0b5b7c3a6380
Sanchari Mandal <sancharim2233@gmail.com>
2026-06-05 23:06:56 +0530

    fix the issue

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,9 +9,9 @@
 
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config({ path: ".env" });
+// config({ path: ".env" });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
