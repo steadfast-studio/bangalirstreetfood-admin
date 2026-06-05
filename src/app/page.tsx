02da640d667704d884d4bf4b71f170224fc7fd64
Sanchari Mandal <sancharim2233@gmail.com>
2026-06-06 01:09:06 +0530

    await headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth.api.getSession();
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   if (!session) {
     redirect("/sign-in");
-  } else {
-    redirect("/dashboard");
   }
+
+  redirect("/dashboard");
 }
